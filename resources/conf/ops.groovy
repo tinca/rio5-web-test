65b75f58f6c37b73dc55f327c00f29c6471d73c1
dreedyman <dennis.reedy@gmail.com>
2015-04-17 10:37:28 -0400

    Use artifacts

--- resources/conf/ops.groovy
+++ resources/conf/ops.groovy
@@ -3,15 +3,20 @@ import org.rioproject.config.Constants
 deployment(name: 'Test') {
     groups System.getProperty(Constants.GROUPS_PROPERTY_NAME, System.getProperty('user.name'))
 
+    artifact id: 'outrigger-dl',   'org.apache.river:outrigger-dl:2.2.2'
+    artifact id: 'outrigger-impl', 'org.apache.river:outrigger:2.2.2'
+
     service(name: 'Outrigger') {
         interfaces {
             classes "net.jini.space.JavaSpace"
-            resources 'test/outrigger-dl-2.2.2.jar'
+            /*resources 'test/outrigger-dl-2.2.2.jar'*/
+            artifact ref: 'outrigger-dl'
         }
         implementation(class: "com.sun.jini.outrigger.TransientOutriggerImpl") {
-            resources 'test/outrigger-2.2.2.jar'
+            /*resources 'test/outrigger-2.2.2.jar'
             resources 'test/outrigger-dl-2.2.2.jar'
-            resources 'test/jsk-dl-2.2.2.jar'
+            resources 'test/jsk-dl-2.2.2.jar'*/
+            artifact ref: 'outrigger-impl'
         }
         maintain 1
         maxPerMachine 1
